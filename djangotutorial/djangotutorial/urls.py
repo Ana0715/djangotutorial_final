5ec62c940bc2906d5374c5dbf73e1cd90bbe182d
Ana0715 <orlovaana0715@gmail.com>
2026-04-28 20:36:33 +0200

    comment

---
+++ djangotutorial/djangotutorial/urls.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from common.views import HealthCheckView, main_page
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Polls API',
+        default_version='v1',
+        description='Polls application documentation',
+    ),
+    permission_classes=[],
+    public=True
+)
+
+urlpatterns = [
+    path('', main_page, name='main_page'),
+    path('healthcheck/', HealthCheckView.as_view(), name='healthcheck'),
+    path("admin/", admin.site.urls),
+    path("api/polls/", include("polls.urls")),
+    path("swagger.<format>/", schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/users/', include('users.urls'))
+]
